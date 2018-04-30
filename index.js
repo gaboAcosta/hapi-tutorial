99eb5149e69839c8548ba5aeb9bfbc5fe360ace1
Gabo Acosta <gabo.acosta624@gmail.com>
2018-04-30 10:26:12 -0500

    Adds the basic server setup

---
+++ index.js
@@ -0,0 +1,21 @@
+const Hapi = require('hapi')
+
+const server = Hapi.server({
+  port: 3000,
+  host: 'localhost',
+  debug: { log: ['*'], request: ['*'] },
+})
+
+server.route({
+  method: 'GET',
+  path: '/',
+  handler(){
+    return 'Hello World!'
+  }
+})
+
+
+server.start()
+.then(() => {
+  console.log(`server running at: ${server.info.uri}`)
+})
